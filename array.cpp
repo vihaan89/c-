f324afd2aee61ff622003e7a30d0f13c6ca981e8
Anshal <anshal570@gmail.com>
2019-10-02 19:14:48 +0530

    Added array program

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//Declaraing an array
+int arr[5]= {1,2,3,4,5};
+
+//Printing an array
+int i;
+for(i=0;i<5;i++){
+	cout<<arr[i]<<"\t";
+}
+
+}
